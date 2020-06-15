6425ae4080b721a750a8cfa4dfcd5a79e6a6c672
a1203991686 <a1203991686@126.com>
2020-06-15 20:14:11 +0800

    实现前台Service

--- TheFirstLineOfCode3/10/ServiceTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/servicetest/MyService.kt
+++ TheFirstLineOfCode3/10/ServiceTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/servicetest/MyService.kt
@@ -1,10 +1,17 @@
 package com.littlecorgi.thefirstlineofcode3.servicetest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class MyService : Service() {
 
@@ -22,6 +29,25 @@ class MyService : Service() {
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "onCreate: 1")
+        // 开启前台Service
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "my_service", "前台Service通知",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(channel)
+        }
+        val intent = Intent(this, MainActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val notification = NotificationCompat.Builder(this, "my_service").apply {
+            setContentTitle("This is content title")
+            setContentText("This is content text")
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_launcher_foreground))
+            setContentIntent(pi)
+        }.build()
+        startForeground(1, notification)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
