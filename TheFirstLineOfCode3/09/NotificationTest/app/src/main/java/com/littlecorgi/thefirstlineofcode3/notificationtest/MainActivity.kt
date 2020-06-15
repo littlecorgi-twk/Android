65038fc738ba4957046071129dcc26bf81644eb2
a1203991686 <a1203991686@126.com>
2020-06-15 16:04:03 +0800

    完成一个纯粹的通知，但是通知无法点击

--- TheFirstLineOfCode3/09/NotificationTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/notificationtest/MainActivity.kt
+++ TheFirstLineOfCode3/09/NotificationTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/notificationtest/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.littlecorgi.thefirstlineofcode3.notificationtest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+
+        sendNotice.setOnClickListener {
+            val notification = NotificationCompat.Builder(this, "normal").apply {
+                setContentTitle("This is content title")
+                setContentText("This is content text")
+                setSmallIcon(R.drawable.ic_launcher_foreground)
+                setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_launcher_foreground))
+            }.build()
+            manager.notify(1, notification)
+        }
     }
 }
