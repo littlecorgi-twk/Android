598e570f20ebacf2e201deac974043abe3cd2343
a1203991686 <a1203991686@126.com>
2020-06-16 19:32:28 +0800

    使用sleep和delay去测试协程输出情况

--- TheFirstLineOfCode3/11/NetworkTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/networktest/coroutines/CoroutinesTest.kt
+++ TheFirstLineOfCode3/11/NetworkTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/networktest/coroutines/CoroutinesTest.kt
@@ -1,5 +1,14 @@
 package com.littlecorgi.thefirstlineofcode3.networktest.coroutines
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 fun main() {
-    
+    GlobalScope.launch {
+        println("codes run in coroutine scope")
+        delay(1500)
+        println("codes run in coroutine scope finished")
+    }
+    Thread.sleep(1000)
 }
