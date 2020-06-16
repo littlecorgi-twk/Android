435224a8ed4d13eb78090af51097cf2ddef32379
a1203991686 <a1203991686@126.com>
2020-06-16 10:31:22 +0800

    完成通过HttpURLConnection进行GET请求

--- TheFirstLineOfCode3/11/NetworkTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/networktest/MainActivity.kt
+++ TheFirstLineOfCode3/11/NetworkTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/networktest/MainActivity.kt
@@ -2,10 +2,58 @@ package com.littlecorgi.thefirstlineofcode3.networktest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sendRequestBtn.setOnClickListener {
+            sendRequestWithHttpUrlConnection()
+        }
+
+        toOkHttpActivityBtn.setOnClickListener {
+            startActivity<OkHttpActivity>(this) {
+                null
+            }
+        }
+    }
+
+    private fun sendRequestWithHttpUrlConnection() {
+        thread {
+            var connection: HttpURLConnection? = null
+            try {
+                val response = StringBuilder()
+                val url = URL("https://www.baidu.com/")
+                connection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+                val input = connection.inputStream
+                val read = BufferedReader(InputStreamReader(input))
+                read.use {
+                    read.forEachLine {
+                        response.append(it)
+                    }
+                }
+                showResponse(response.toString())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+        }
+    }
+
+    private fun showResponse(response: String) {
+        runOnUiThread {
+            responseText.text = response
+        }
     }
 }
---
+++ TheFirstLineOfCode3/11/NetworkTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/networktest/OkHttpActivity.kt
@@ -0,0 +1,12 @@
+package com.littlecorgi.thefirstlineofcode3.networktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OkHttpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ok_http)
+    }
+}
