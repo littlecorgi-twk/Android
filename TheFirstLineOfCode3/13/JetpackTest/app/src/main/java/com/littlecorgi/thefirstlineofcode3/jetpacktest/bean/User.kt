09c8f76459485d978ab1c28400365e414ac41f0e
a1203991686 <a1203991686@126.com>
2020-06-17 15:09:00 +0800

    使用map函数

---
+++ TheFirstLineOfCode3/13/JetpackTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/jetpacktest/bean/User.kt
@@ -0,0 +1,3 @@
+package com.littlecorgi.thefirstlineofcode3.jetpacktest.bean
+
+data class User(var firstName: String, var lastName: String, var age: Int)
--- TheFirstLineOfCode3/13/JetpackTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/jetpacktest/viewmodel/MainViewModel.kt
+++ TheFirstLineOfCode3/13/JetpackTest/app/src/main/java/com/littlecorgi/thefirstlineofcode3/jetpacktest/viewmodel/MainViewModel.kt
@@ -2,13 +2,21 @@ package com.littlecorgi.thefirstlineofcode3.jetpacktest.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.littlecorgi.thefirstlineofcode3.jetpacktest.bean.User
 
 class MainViewModel(countReserved: Int) : ViewModel() {
     val counter: LiveData<Int>
         get() = _counter
 
     private val _counter = MutableLiveData<Int>()
+
+    private val userLiveData = MutableLiveData<User>()
+
+    val userName = Transformations.map(userLiveData) { user ->
+        "${user.firstName} ${user.lastName}"
+    }
 
     init {
         _counter.value = countReserved
