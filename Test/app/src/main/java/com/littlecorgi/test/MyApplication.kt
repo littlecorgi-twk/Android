18a446cdde52bb9e1f1951d863c01e7f4d463c61
a1203991686 <a1203991686@126.com>
2020-09-09 22:00:59 +0800

    🔧feat:EventBusTest添加索引加速(Index)(注解处理器)

--- Test/app/src/main/java/com/littlecorgi/test/MyApplication.kt
+++ Test/app/src/main/java/com/littlecorgi/test/MyApplication.kt
@@ -2,6 +2,7 @@ package com.littlecorgi.test
 
 import android.app.Application
 import android.content.Context
+import org.greenrobot.eventbus.EventBus
 
 class MyApplication : Application() {
 
@@ -12,5 +13,7 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         context = this
+        // val eventBus = EventBus.builder().addIndex(MyEventBusIndex()).build()
+        EventBus.builder().addIndex(MyEventBusIndex()).installDefaultEventBus()
     }
 }
