ef825c6dd6f68e15d8594ceb2cd312683f489188
a1203991686 <a1203991686@126.com>
2020-11-17 21:39:15 +0800

    feat:更新为组件化

---
+++ Test/common/src/main/java/com/littlecorgi/common/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.littlecorgi.common
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+}
